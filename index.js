f732342027100bc87ec6f8eae969c935658864f5
Rose <forme1415@gmail.com>
2024-07-05 09:46:16 +0800

    第一版程式

---
+++ index.js
@@ -0,0 +1,64 @@
+/**
+* common js模組寫法
+*/
+
+const express = require("express");
+// 導入json
+// const data = require("./singer.json");
+const {singers} = require("./singer.json");
+console.log(singers);
+
+const portNum = 3000;
+const app = express();
+
+/* --- 路由規則 --- */
+app.get("/",(req,res) => {
+    // res.send("這是首頁");
+    res.redirect("/singer/3.html");
+});
+
+app.get("/singer/:id.html",(req,res) => {
+    const id = parseInt(req.params.id);
+    const result = singers.find( singer => {
+        if(singer.id === id){
+            return true;
+        }
+    });
+    // 寫法一
+    // if(result){
+
+    // }else{
+
+    // }
+
+    // 寫法二
+    if(!result){
+        // 沒有結果
+        res.statusCode = 404;
+        res.send("<h1>404-找不到歌手</h1>")
+        return;
+    }
+    // 有結果
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${result.singer_name}</title>
+</head>
+<body>
+    <h1>${result.singer_name}</h1>
+     <h3>${result.singer_id}</h3>
+    <img src="${result.singer_img}" alt="">
+</body>
+</html>`);
+});
+
+app.all("*",(req,res) => {
+    res.send("<h1>404-找不到</h1>")
+})
+
+app.listen(3000,() => {
+    console.log(`服務已啟動於http://localhost:3000`);
+})
+
---
+++ test.mjs
@@ -0,0 +1,8 @@
+/**
+*  ES6 模組
+*/
+
+/* --- asset斷言再指定json --- */
+import data from "./singer.json" assert{type: "json"};
+
+console.log(data);
